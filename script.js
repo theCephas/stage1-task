8a67eb99fcde59f41457027e971b6b7d3c4fad3b
theCephas <oshoiseoluwa@gmail.com>
2023-09-07 16:57:25 +0100

    Initial Commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// Getting the current date of the week
+
+const currentDate = new Date();
+
+const dayOfWeek = currentDate.getDay();
+const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const currentDay = daysOfWeek[dayOfWeek];
+
+// Targeting the vacant space for the current day of the week
+
+const theDay = document.getElementById("currentDay");
+theDay.innerHTML = currentDay;
+
+// Getting the current UTC time 
+
+const currentUTC = new Date();
+const currentTimeInMilliseconds = currentUTC.getTime();
+
+// Targeting the vacant space for the current UTC time
+
+const theCurrentUTC = document.getElementById("currentUTC");
+theCurrentUTC.innerHTML = currentTimeInMilliseconds; 
